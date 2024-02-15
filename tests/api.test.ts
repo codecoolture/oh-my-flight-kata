661801c2e2d75aec76fb23f7622a3da5ac2f8d44
Sergio Álvarez <sergio@codecoolture.com>
2024-02-15 12:12:11 +0100

    Use async/await instead of callbacks

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { api } from "../src/api";
 
 describe("GET /", () => {
-  it("works", (done) => {
-    request(api).get("/").expect(200, done);
+  it("works", async () => {
+    await request(api).get("/").expect(200);
   });
 });
